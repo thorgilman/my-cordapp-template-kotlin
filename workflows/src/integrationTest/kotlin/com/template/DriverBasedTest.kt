088e9913b175bd992e1548f4fa3eaa4ca0db46a6
thorgilman <thorgilman@gmail.com>
2021-05-12 16:23:17 -0400

    Added helper functions for querying states

--- workflows/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
+++ workflows/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
@@ -3,10 +3,19 @@ package com.template
 import com.template.flows.Initiator
 import com.template.states.DataState
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.messaging.startFlow
+import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.messaging.vaultTrackBy
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.PageSpecification
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.utilities.getOrThrow
 import net.corda.node.services.Permissions
 import net.corda.testing.core.TestIdentity
@@ -14,6 +23,7 @@ import net.corda.testing.driver.DriverDSL
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
+import net.corda.testing.node.StartedMockNode
 import net.corda.testing.node.User
 import org.junit.Test
 import java.util.concurrent.Future
@@ -35,6 +45,14 @@ class DriverBasedTest {
         assertEquals(1, bProxy.vaultQuery(DataState::class.java).states.size)
     }
 
+    inline fun <reified T: ContractState> CordaRPCOps.getStates(): List<ContractState> {
+        return this.vaultQueryBy<T>().states.map{it.state.data}
+    }
+
+    inline fun <reified T: LinearState> CordaRPCOps.getStateByLinearId(linearId: UniqueIdentifier): StateAndRef<T> {
+        return this.vaultQueryBy<T>(QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))).states[0]
+    }
+
     private fun CordaRPCOps.identity(): Party = this.nodeInfo().legalIdentities[0]
 
     private fun DriverDSL.setupNode(x500Name: CordaX500Name): CordaRPCOps {
@@ -45,6 +63,8 @@ class DriverBasedTest {
         val proxy = client.start(username, "password").proxy
         return proxy
     }
+
+
 
     // Runs a test inside the Driver DSL, which provides useful functions for starting nodes, etc.
     private fun withDriver(test: DriverDSL.() -> Unit) = driver(
